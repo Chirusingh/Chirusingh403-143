2183099cbaba9644fc23d5140b5825fece6feb95
Chirusingh <chirusingh403@gmail.com>
2021-01-24 20:43:43 +0530

    Added 2 more print statements -Chiru

--- src/test/java/FirstProgram.java
+++ src/test/java/FirstProgram.java
@@ -4,6 +4,8 @@ public class FirstProgram
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello ");
+		System.out.println("Hello India ");
+		System.out.println("Hello Bangalore");
 	}
 
 }
